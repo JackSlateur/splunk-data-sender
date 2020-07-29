3c484d14d45254d336e035c2e9f8e47d3be1388b
Andrea Salvatori - IU6FZL <andrea.salvatori92@gmail.com>
2020-07-29 23:21:28 +0200

    Update to versione 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='splunk_data_sender',
-    version='0.0.1',
+    version='0.0.3',
     license='MIT License',
     description='A Python logging handler that sends your logs to Splunk',
     long_description=open('README.md').read(),
@@ -11,7 +11,7 @@ setup(
     author_email='andrea.salvatori92@gmail.com',
     url='https://github.com/Sonic0/splunk-data-sender',
     packages=find_packages(),
-    install_requires=['requests >= 2.24.0, < 3.0.0'],
+    install_requires=['requests >= 2.5.0, < 3.0.0'],
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
@@ -19,10 +19,11 @@ setup(
         'Natural Language :: English',
         'Operating System :: POSIX :: Linux',
         'Operating System :: MacOS :: MacOS X',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Topic :: System :: Logging'
-    ]
+    ],
+    python_requires='>=3.6',
 )
