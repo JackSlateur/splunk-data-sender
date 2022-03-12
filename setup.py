854a8d277b772a0f07bc4f5cda09ed8a59d35775
Andrea Salvatori <andrea.salvatori92@gmail.com>
2022-03-12 19:58:16 +0100

    Bumped version to 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='splunk_data_sender',
-    version='0.2.0',
+    version='0.3.0',
     license='MIT License',
     description='A Python connector that sends your data to Splunk',
     long_description=open('README.md').read(),
